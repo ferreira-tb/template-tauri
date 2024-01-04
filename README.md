f9d4f06cd2e6d0ad09cca6543c7ef10a9f2be7ce
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-04 14:44:19 -0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+```bash
+cargo install manatsu
+manatsu create
+```
+
+## License
+
+[MIT](https://raw.githubusercontent.com/manatsujs/manatsu/main/LICENSE)
+
+Copyright (c) 2023 Andrew Ferreira
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dev from 'vite-plugin-vue-devtools';
+
+export default defineConfig({
+  plugins: [vue(), dev()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      ignored: ['**/src-tauri/**']
+    }
+  }
+});
