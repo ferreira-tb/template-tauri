612ea6cde7dbc07b69f16460829298ecb5e87fea
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-21 23:02:36 -0300

    chore: bump version

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { URL, fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import dev from 'vite-plugin-vue-devtools';
@@ -5,11 +6,20 @@ import dev from 'vite-plugin-vue-devtools';
 export default defineConfig({
   plugins: [vue(), dev()],
   clearScreen: false,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('src', import.meta.url))
+    }
+  },
   server: {
     port: 1420,
     strictPort: true,
     watch: {
       ignored: ['**/src-tauri/**']
     }
+  },
+  build: {
+    emptyOutDir: true,
+    minify: false
   }
 });
