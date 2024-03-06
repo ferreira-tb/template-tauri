ef909c3cdc100cb2e76d5421d80c7dcb8568a223
Andrew Ferreira <andrew.shien2@gmail.com>
2024-03-06 00:19:53 -0300

    chore: bump version

---
+++ src-tauri/src/database.rs
@@ -0,0 +1,10 @@
+// pub mod entities;
+
+// pub use entities::prelude::*;
+use sea_orm::prelude::DatabaseConnection;
+use sea_orm::Database;
+
+pub async fn connect() -> DatabaseConnection {
+  const DATABASE_URL: &str = "sqlite::memory:";
+  Database::connect(DATABASE_URL).await.unwrap()
+}
