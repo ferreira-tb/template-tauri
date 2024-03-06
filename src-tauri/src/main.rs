76a2d2a39542abcb105783563115d69c0b112538
Andrew Ferreira <andrew.shien2@gmail.com>
2024-03-06 00:19:39 -0300

    chore: bump version

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![allow(async_fn_in_trait)]
 
 mod command;
+mod database;
 mod error;
 
-use sea_orm::prelude::DatabaseConnection;
-use sea_orm::Database;
 pub use error::{Error, Result};
-
-const DATABASE_URL: &str = "sqlite::memory:";
+use sea_orm::prelude::DatabaseConnection;
 
 pub struct AppState {
   pub db: DatabaseConnection,
@@ -16,12 +15,13 @@ pub struct AppState {
 
 #[tokio::main]
 async fn main() {
-  let db = Database::connect(DATABASE_URL).await.unwrap();
+  let db = database::connect().await;
+  let state = AppState { db };
 
   tauri::Builder::default()
     .plugin(tauri_plugin_window_state::Builder::default().build())
     .plugin(tauri_plugin_manatsu::init())
-    .manage(AppState { db })
+    .manage(state)
     .invoke_handler(tauri::generate_handler![command::version])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
