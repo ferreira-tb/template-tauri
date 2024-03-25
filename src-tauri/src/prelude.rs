36d3fd16586c3ba56a284b7a41a4509bb9df6716
Andrew Ferreira <andrew.shien2@gmail.com>
2024-03-24 21:55:04 -0300

    chore: bump version

--- src-tauri/src/prelude.rs
+++ src-tauri/src/prelude.rs
@@ -1,5 +1,6 @@
 pub use crate::error::{Error, Result};
 pub use crate::state::{AppState, State};
 pub use anyhow::anyhow;
+pub use itertools::Itertools;
 pub use std::thread;
 pub use tauri::{async_runtime, AppHandle, Manager, Runtime};
